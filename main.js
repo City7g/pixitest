14caf474c0a744e5643ad602b3aa2b7a6e4ebf33
City7g <City7gor@gmail.com>
2024-11-15 18:22:21 +0200

    change path pixi

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as PIXI from 'pixi.js'
+import * as PIXI from './node_modules/pixi.js/lib/index'
 import '@pixi/filter-displacement'
 import fragmentShader from './anim1.glsl?raw'
 import gsap from 'gsap'
